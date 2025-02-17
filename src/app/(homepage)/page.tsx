f5ff5dafa740de50ba673a49ac656c35255c9ad8
Rishabh Sandhu <171568557+threesann@users.noreply.github.com>
2025-02-17 20:12:58 +0000

    added mobile scaling, sections & cool (buggy) screen scroll feature :D

--- src/app/(homepage)/page.tsx
+++ src/app/(homepage)/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
+
+import Descriptions from '@/lib/homepage/sitedescriptions';
+
 
 export default function Home() {
     interface RotateElementParams {
@@ -9,13 +12,50 @@ export default function Home() {
     }
 
     const [showDesc, setShowDesc] = useState<string>("");
+    const currentDescription = Descriptions.find(desc => desc.name === showDesc);
+
+    
 
     useEffect(() => {
         const rotated = document.getElementById('rotate');
         document.addEventListener('mousemove', (e) => {
             rotateElement({ event: e, element: rotated as HTMLElement });
         });
+
+        // scroll handling
+        const sections = [
+            document.querySelector('.section1'),
+            document.querySelector('.section2'),
+        ]
+        const prevScrollY = {current : 0}
+        let currentSection = 0;
+
+        const handleScroll = () => {
+            const currentScrollY = window.scrollY;
+            if (currentScrollY > prevScrollY.current) {
+                console.log('Scrolling down');
+                currentSection = Math.min(currentSection + 1, sections.length - 1);
+                sections[currentSection]?.scrollIntoView({ behavior: 'smooth', block: 'start' });
+            } else {
+                console.log('Scrolling up');
+                currentSection = Math.max(currentSection - 1, 0);
+                sections[currentSection]?.scrollIntoView({ behavior: 'smooth', block: 'start' });
+            }
+            prevScrollY.current = currentScrollY;
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+        
+
     }, []);
+
+
+
+    
 
     function rotateElement({ event, element }: RotateElementParams) { // event represents mouse movement, element is the element to rotate
         const x = event.clientX; // get the horizontal mouse position
@@ -33,60 +73,33 @@ export default function Home() {
     }
 
     return (
-        <div className="w-full h-full bg-nighttime animate-backgroundMove">
-            <div className="flex items-center justify-center h-full w-1/5 m-auto">
+        <div className='flex flex-col'>
+        {/* section 1 */}
+        <div className="section1 w-full h-[98vh] bg-nighttime animate-backgroundMove">
+            <div className="flex items-center justify-center h-full md:w-1/5 w-4/5 m-auto">
 
                 {/* change number in style of container div to change no. of elements in circle - other settings such as radius/space between elements are in globals.css */}
                 {/* circle div (container) */}
-                <div className="container" style={{"--m": 6} as React.CSSProperties}>
-
+                <div className="circle-container flex flex-col gap-1" style={{"--m": 6} as React.CSSProperties}>
                     {/* central element - properties of rotation can be changed above, styling in globals.css */}
                     <div id="rotate" className="flex flex-col items-center p-4 h-fit w-full bg-theme-background border-4 border-theme-border shadow shadow-black/50 animate-slideUpAndFade">
                         <p className="text-4xl pb-2">threesan</p>
-                        {showDesc === "" &&
-                            <p className="text-center">this is my website to display everything i've ever made ever.</p>
-                        }
-                        {showDesc === "chatitin" && 
+                        {currentDescription ?
                             <div className='flex flex-col w-4/5'>
-                                <img src="/pic_chatitin.png" className='border-4 border-theme-border' />
-                                <p className='text-4xl'>CHAT IT IN</p>
-                                <p className='text-sm'>Released: 16/9/2024</p>
-                                <p>A chatroom for Very Special People.</p>
-                                <p className='text-xs'><br />Developed by David Fiddes, Designed by threesan</p>
+                                <img src={currentDescription.image} className='border-4 border-theme-border' />
+                                <p className='text-4xl'>{currentDescription.title}</p>
+                                <p className='text-sm'>Released: {currentDescription.releaseDate}</p>
+                                <p>{currentDescription.description}</p>
+                                <p className='text-xs'><br />{currentDescription.credits}</p>
                             </div>
-                        }
-                        {showDesc === "threesangithub" && 
-                            <div className='flex flex-col w-4/5'>
-                                <img src="/pic_threesan.png" className='border-4 border-theme-border' />
-                                <p className='text-4xl'>threesann.github.io</p>
-                                <p className='text-sm'>Released: 3/6/2024</p>
-                                <p>My old portfolio website. It's like a simpler version of this one.</p>
-                                <p className='text-xs'><br />Developed by threesan</p>
-                            </div>
-                        }
-                        {showDesc === "kevsterclicker" && 
-                            <div className='flex flex-col w-4/5'>
-                                <img src="/pic_kevsterclicker.png" className='border-4 border-theme-border' />
-                                <p className='text-4xl'>Kevster Clicker</p>
-                                <p className='text-sm'>Released: 10/6/2024</p>
-                                <p>Perhaps the greatest & most controversial game of All Time.</p>
-                                <p className='text-xs'><br />Developed by David Fiddes, Designed by threesan</p>
-                            </div>
-                        }
-                        {showDesc === "nighttimeforest" && 
-                            <div className='flex flex-col w-4/5'>
-                                <img src="/pic_nighttimeforest.png" className='border-4 border-theme-border' />
-                                <p className='text-4xl'>??????</p>
-                                <p className='text-sm'>Released: ??/??/????</p> 
-                                <p>Who knows what it could be?</p>
-                                <p className='text-xs'><br />Developed by threesan</p>
-                            </div>
+
+                            : <p className="text-center">this is my website to display everything i've ever made ever.</p>
                         }
                     </div>
 
                     {/* elements in circle; 1-6 */}
                     <div style={{"--i": 1} as React.CSSProperties}>
-                        <div className="animate-slideUpAndFadeAndBob bg-theme-background border-4 border-theme-border hover:border-white hover:cursor-not-allowed shadow shadow-black/50">
+                        <div className="md:block hidden animate-slideUpAndFadeAndBob bg-theme-background border-4 border-theme-border hover:border-white hover:cursor-not-allowed shadow shadow-black/50">
                             <img src="/icon_comingsoon.png" className="w-full h-full"/>
                         </div>
                     </div>
@@ -134,14 +147,24 @@ export default function Home() {
                     </div>
 
                     <div style={{"--i": 6} as React.CSSProperties}>
-                        <div className="animate-slideUpAndFadeAndBob bg-theme-background border-4 border-theme-border hover:border-white hover:cursor-not-allowed shadow shadow-black/50">
+                        <div className="md:block hidden animate-slideUpAndFadeAndBob bg-theme-background border-4 border-theme-border hover:border-white hover:cursor-not-allowed shadow shadow-black/50">
                             <img src="/icon_comingsoon.png" className="w-full h-full"/>
                         </div>
                     </div>
 
                 </div>  
             </div>
-            <p className='absolute bottom-1 w-full text-center text-theme-border text-sm'>© 2025, burgerbross Sp. z o.o. (not a real company)</p>
+            <p className='absolute bottom-0 w-full text-center text-theme-border text-sm'>© 2025, burgerbross Sp. z o.o. (not a real company)</p>
+        </div>
+        {/* section 2 */}
+        <div className='section2 w-full h-screen bg-theme-background'>
+            <div className='flex flex-col items-center justify-center w-3/5 h-full m-auto text-center gap-1'>
+                <p className='text-4xl'>hiya :]</p>
+                <p className='text-2xl'>this section is currently under development. i just made this cool scroll feature but don't have the time to learn how to make a beta branch for my github repo.
+                    if anyone sees this, then now you know!
+                </p>
+            </div>
+        </div>
         </div>
       
     );
---
+++ src/lib/homepage/sitedescriptions.ts
@@ -0,0 +1,40 @@
+
+
+
+const Descriptions = [
+    {
+        name: "chatitin",
+        image: "/pic_chatitin.png",
+        title: "CHAT IT IN",
+        releaseDate: "16/9/2024",
+        description: "A chatroom for Very Special People.",
+        credits: "Developed by David Fiddes, Designed by threesan",
+    },
+    {
+        name: "threesangithub",
+        image: "/pic_threesan.png",
+        title: "threesann.github.io",
+        releaseDate: "3/6/2024",
+        description: "My old portfolio website. It's like a simpler version of this one, with a bunch of fun old stuff I made.",
+        credits: "Developed by threesan",
+    },
+    {
+        name: "kevsterclicker",
+        image: "/pic_kevsterclicker.png",
+        title: "Kevster Clicker",
+        releaseDate: "10/6/2024",
+        description: "Perhaps the greatest & most controversial game of All Time.",
+        credits: "Developed by David Fiddes, Designed by threesan",
+    },
+    {
+        name: "nighttimeforest",
+        image: "/pic_nighttimeforest.png",
+        title: "??????",
+        releaseDate: "??/??/????",
+        description: "Who knows what it could be?",
+        credits: "Developed by threesan",
+    },
+];
+
+
+export default Descriptions;
